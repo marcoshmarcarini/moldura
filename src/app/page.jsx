f552706bb3e2da6c2bc07da156af364aa1d5aaad
marcoshmarcarini <marcoshmarcarini@hotmail.com>
2024-09-16 17:15:50 -0300

    Update page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,12 +1,10 @@
 'use client'
 
-import MergeImage from "@/components/Peter/MergeImage/page"
-
 export default function Home() {
-  
+
   return (
     <div className={`flex items-center justify-center  p-5`}>
-      <MergeImage />
+      Página Inicial
     </div>
   )
 }
