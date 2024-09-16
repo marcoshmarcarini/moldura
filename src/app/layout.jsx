a45746ef871286b023bad11a579b3c38eeb91f35
marcoshmarcarini <marcoshmarcarini@hotmail.com>
2024-09-16 17:09:48 -0300

    Update

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -5,18 +5,12 @@ import "./globals.css"
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Crie sua Moldura",
-  description: "Aplicação para criação de moldura para fotos de perfil.",
   authors: {name: "Marcos Henrique Marcarini Junior"}
-};
+}
 
 export default function RootLayout({ children }) {
   return (
     <html lang="pt-br">
-      <head>
-        <title>To com o Peter</title>
-        <link rel="icon" href="/favicon-peter.ico" />
-        </head>
       <body className={`${inter.className}`}>{children}</body>
     </html>
   );
--- src/app/peter/layout.jsx
+++ src/app/peter/layout.jsx
@@ -1,6 +1,10 @@
 export default function PeterLayout({ children }) {
     return (
         <html lang="pt-br">
+            <head>
+                <title>To com Peter</title>
+                <link rel="icon" href="/favicon-peter.ico" sizes="any"/>
+            </head>
             <body style={{overflow: 'hidden'}}>
                 {children}
             </body>
