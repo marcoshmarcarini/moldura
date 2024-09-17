ae7d9ff8c74c51c8471ebe91e51368c6c63855b5
marcoshmarcarini <marcoshmarcarini@hotmail.com>
2024-09-17 10:24:38 -0300

    Update layout.jsx

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -4,139 +4,69 @@ import "./globals.css"
 import { useEffect, useState } from "react"
 import { useRouter } from "next/navigation"
 
-
 const inter = Inter({ subsets: ["latin"] });
-
-/* export const metadata = {
-  authors: {name: "Marcos Henrique Marcarini Junior"}
-} */
-
 
 export default function RootLayout({ children }) {
   const [title, setTitle] = useState("Entre na Campanha")
+  const [ogTitle, setOgTitle] = useState("Entre na Campanha")
+  const [ogDescription, setOgDescription] = useState("Participe da nossa campanha!")
+  const [ogImage, setOgImage] = useState("/default-image.jpg")
   const router = useRouter()
-  console.log(router.pathname)
-
 
   useEffect(() => {
-    handleRouter()
-
-  }, [router.pathname])
-
-
-  const handleRouter = () => {
     switch (router.pathname) {
-      case "/peter" || "/peter/moldura":
+      case "/peter":
+      case "/peter/moldura":
         setTitle("To com Peter")
-        return (
-          <html lang="pt-br">
-            <head>
-              <title>
-                {title}
-              </title>
-              <link
-                rel="icon"
-                href="/favicon-peter.ico"
-                sizes="any"
-              />
-              <meta name="authors" content="Marcos Henrique Marcarini Junior" />
-            </head>
-            <body
-              className={`${inter.className}`}
-              style={{ overflow: 'hidden' }}>
-              {children}
-            </body>
-          </html>
-        )
+        setOgTitle("To com Peter")
+        setOgDescription("Apoie Peter em sua jornada!")
+        setOgImage("/images/peter-campanha.jpg")
         break
-      case "/dorlei" || "/dorlei/moldura":
+      case "/dorlei":
+      case "/dorlei/moldura":
         setTitle("To com Dorlei")
-        return (
-          <html lang="pt-br">
-            <head>
-              <title>
-                {title}
-              </title>
-              <link
-                rel="icon"
-                href="/favicon-dorlei.ico"
-                sizes="any"
-              />
-              <meta name="authors" content="Marcos Henrique Marcarini Junior" />
-            </head>
-            <body
-              className={`${inter.className}`}
-              style={{
-                overflow: 'hidden',
-                background: 'linear-gradient(to bottom,transparent,var(--f-azul-claro))var(--f-azul-escuro)',
-              }}>
-              {children}
-            </body>
-          </html>
-        )
+        setOgTitle("To com Dorlei")
+        setOgDescription("Apoie Dorlei para as eleições de 2024!")
+        setOgImage("/images/dorlei-campanha.jpg")
         break
-      case "/ferraco" || "ferraco/moldura":
-        setTitle("To com Ferraco")
-        return (
-          <html lang="pt-br">
-            <head>
-              <title>
-                {title}
-              </title>
-              <link
-                rel="icon"
-                href="/favicon-ferraco.ico"
-                sizes="any"
-              />
-
-              <meta name="authors" content="Marcos Henrique Marcarini Junior" />
-              <meta property="og:type" content="website" />
-              <meta property="og:url" content="https://ferraco11.com.br" />
-              <meta property="og:title" content="To com Ferraço" />
-              <meta property="og:description" content="Apoie Theodorico Ferraço para as eleições de 2024 em Cachoeiro de Itapemirim. Entre na campanha!" />
-              <meta property="og:image" content="https://www.entrenacampanha.com.br/favicon-ferraco.ico" />
-
-            </head>
-            <body
-              className={`${inter.className}`}
-              style={{
-                overflow: 'hidden',
-                background: 'linear-gradient(to bottom,transparent,var(--f-azul-claro))var(--f-azul-escuro)',
-              }}>
-              {children}
-            </body>
-          </html>
-        )
+      case "/ferraco":
+      case "/ferraco/moldura":
+        setTitle("To com Ferraço")
+        setOgTitle("To com Ferraço")
+        setOgDescription("Apoie Theodorico Ferraço para as eleições de 2024!")
+        setOgImage("/images/ferraco-campanha.jpg")
         break
       default:
-        return (
-          <html lang="pt-br">
-            <head>
-              <title>
-                {title}
-              </title>
-              <meta name="authors" content="Marcos Henrique Marcarini Junior" />
-            </head>
-            <body className={`${inter.className}`}>
-              {children}
-            </body>
-          </html>
-        )
-        break
+        setTitle("Entre na Campanha")
+        setOgTitle("Entre na Campanha")
+        setOgDescription("Participe da nossa campanha!")
+        setOgImage("/default-image.jpg")
     }
-  }
+  }, [router.pathname])
 
   return (
-    handleRouter()
-  )
-
-  /* return (
     <html lang="pt-br">
       <head>
-        <title>Entre na campanha</title>
+        <title>{title}</title>
+        <meta name="authors" content="Marcos Henrique Marcarini Junior" />
 
+        {/* Meta tags Open Graph */}
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content={`https://entrenacampanha.com.br/${router.pathname}`} />
+        <meta property="og:title" content={ogTitle} />
+        <meta property="og:description" content={ogDescription} />
+        <meta property="og:image" content={ogImage} />
+
+        {/* Twitter Cards */}
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content={`https://entrenacampanha.com.br/${router.pathname}`} />
+        <meta property="twitter:title" content={ogTitle} />
+        <meta property="twitter:description" content={ogDescription} />
+        <meta property="twitter:image" content={ogImage} />
       </head>
-      <body className={`${inter.className}`}>{children}</body>
+      <body className={inter.className}>
+        {children}
+      </body>
     </html>
-  ); */
+  )
 }
